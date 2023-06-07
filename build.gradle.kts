58729a6ea358fcbf58d5b646eeca49d78a672e17
tbm98 <tbm98.dev@gmail.com>
2023-06-07 23:41:39 +0700

    up2

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,14 +76,8 @@ tasks {
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-            val start = "<!-- Plugin description -->"
-            val end = "<!-- Plugin description end -->"
-
             with (it.lines()) {
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+                joinToString("\n").let(::markdownToHTML)
             }
         }
 
